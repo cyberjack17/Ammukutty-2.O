b34c438d0ec1f82443aa1cb0682eb3b5b09f32ad
ᴄʏʙᴇʀ㊌ᴊᴀᴄᴋ <92788624+cyberjack17@users.noreply.github.com>
2021-10-23 23:09:14 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*NANDHUTTY_V2 by AJMAL AND ACHU*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*AMMUKUTTY-2.O by ᴄʏʙᴇʀ㊌ᴊᴀᴄᴋ*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*NANDHUTTY_V2 by AJMAL AND ACHU*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*AMMUKUTTY-2.O by ᴄʏʙᴇʀ㊌ᴊᴀᴄᴋ*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
